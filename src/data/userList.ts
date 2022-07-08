1e6c3b0e69d2b3d386173f7d3e35c95356a053ae
Dario Hunjadi <dario.hunjadi@hotmail.com>
2022-07-08 14:56:31 +0200

    created userList

---
+++ src/data/userList.ts
@@ -0,0 +1,72 @@
+interface IBooks {
+    id: string;
+    author: string;
+    country: string;
+    imageLink: string;
+    language: string;
+    link: string;
+    pages: number;
+    title: string;
+    year: number;
+    copiesAvailiable: number;
+}
+
+interface IClasses {
+    title: string;
+    semester: number;
+    exam1: number;
+    exam2: number;
+    essay: number;
+    presentation: number;
+    ects: number;
+}
+
+export interface IUser {
+    id: string;
+    fName: string;
+    lName: string;
+    email: string;
+    password: string;
+    status: string;
+    studyProgram?: string;
+    year?: number;
+    books?: IBooks[];
+    classes?: IClasses[];
+}
+
+const userList: IUser[] = [
+    {
+        id: '1',
+        fName: 'Ivan',
+        lName: 'Ivanic',
+        email: 'iivanic@vuka.hr',
+        password: 'vukastudent',
+        status: 'student',
+        studyProgram: 'hospitality',
+        year: 2,
+        books: [],
+        classes: [],
+    },
+    {
+        id: '2',
+        fName: 'Marko',
+        lName: 'Markovic',
+        email: 'mmarkovic@vuka.hr',
+        password: 'vukaprof',
+        status: 'professor',
+        books: [],
+        classes: [],
+    },
+    {
+        id: '3',
+        fName: 'Admin',
+        lName: 'Adminic',
+        email: 'admin@vuka.hr',
+        password: 'vukaadmin',
+        status: 'admin',
+        books: [],
+        classes: [],
+    },
+];
+
+export default userList;
