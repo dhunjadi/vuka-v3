98362e7cf96ea5e3c6d807eb5a32c99af7c5694b
Dario Hunjadi <dario.hunjadi@hotmail.com>
2022-08-17 17:16:25 +0200

    changed color variable names, added basic styling to Book component

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -8,7 +8,37 @@ const Book = (): JSX.Element => {
 
     const {id} = useParams();
     const selectedBook = bookList.find((news) => news.id === id);
-    return <div className="c-tabs">{selectedBook?.title}</div>;
+    return (
+        <div className="c-book">
+            <div className="c-book__img">
+                <img src={selectedBook?.imgSrc} alt="" />
+            </div>
+
+            <div className="c-book__info">
+                <div className="c-book__info_title">
+                    Title: <p>{selectedBook?.title}</p>
+                </div>
+                <div>
+                    Author: <p>{selectedBook?.author}</p>
+                </div>
+                <div>
+                    Language: <p>{selectedBook?.language}</p>
+                </div>
+                <div>
+                    Pages: <p>{selectedBook?.pages}</p>
+                </div>
+                <div>
+                    Year: <p>{selectedBook?.year}</p>
+                </div>
+                <div>
+                    Copies Availiable: <p>{selectedBook?.copiesAvailiable}</p>
+                </div>
+                <div className="c-book__btns">
+                    <button className="btn btn--primary">Make a reservation</button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Book;
